3449c1a35192878dcd0dcbc99023cde9da374f66
James Wonsever <james.wonsever@alphaflow.com>
2019-06-03 10:38:08 -0700

    move test back to pop package

--- meta_type_test.go
+++ meta_type_test.go
@@ -1,10 +1,9 @@
-package pop_test
+package pop
 
 import (
 	"reflect"
 	"testing"
 
-	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/pop/nulls"
 	"github.com/stretchr/testify/require"
 )
@@ -12,7 +11,7 @@ import (
 func Test_Model_Meta(t *testing.T) {
 	r := require.New(t)
 
-	m := pop.Model{Value: &User{}}
+	m := Model{Value: &User{}}
 	mm := m.Meta()
 
 	r.Equal(mm.Type, reflect.TypeOf(m.Value))
@@ -22,7 +21,7 @@ func Test_Model_Meta(t *testing.T) {
 func Test_Model_Meta_Slice(t *testing.T) {
 	r := require.New(t)
 
-	m := pop.Model{Value: &User{}}
+	m := Model{Value: &User{}}
 	mm := m.Meta()
 	sl := mm.MakeSlice()
 
@@ -34,7 +33,7 @@ func Test_Model_Meta_Map_For_Struct(t *testing.T) {
 	r := require.New(t)
 
 	u := User{}
-	m := pop.Model{Value: &u}
+	m := Model{Value: &u}
 	mm := m.Meta()
 	sl := mm.MakeMap()
 
@@ -46,9 +45,9 @@ func Test_Model_Meta_Map_For_Slice(t *testing.T) {
 	r := require.New(t)
 
 	u := []*User{
-		&User{Email: "user@email.com"},
+		{Email: "User@email.com"},
 	}
-	m := pop.Model{Value: &u}
+	m := Model{Value: &u}
 	mm := m.Meta()
 	sl := mm.MakeMap()
 
@@ -58,7 +57,7 @@ func Test_Model_Meta_Map_For_Slice(t *testing.T) {
 	// Map for non-struct with pointer.
 	n := 1
 	v := []*int{&n}
-	m = pop.Model{Value: &v}
+	m = Model{Value: &v}
 	mm = m.Meta()
 	sl = mm.MakeMap()
 	r.Equal(sl.Type, reflect.MapOf(reflect.TypeOf(""), reflect.TypeOf(v[0])))
@@ -66,7 +65,7 @@ func Test_Model_Meta_Map_For_Slice(t *testing.T) {
 
 	// Map for non-struct without pointer.
 	v2 := []int{1}
-	m = pop.Model{Value: &v2}
+	m = Model{Value: &v2}
 	mm = m.Meta()
 	sl = mm.MakeMap()
 	r.Equal(sl.Type, reflect.MapOf(reflect.TypeOf(""), reflect.PtrTo(reflect.TypeOf(v2[0]))))
@@ -76,7 +75,7 @@ func Test_Model_Meta_Map_For_Slice(t *testing.T) {
 func Test_Model_Meta_Associations(t *testing.T) {
 	r := require.New(t)
 
-	m := pop.Model{Value: &User{}}
+	m := Model{Value: &User{}}
 	mm := m.Meta()
 
 	mAssociations := mm.Associations()
@@ -84,56 +83,56 @@ func Test_Model_Meta_Associations(t *testing.T) {
 }
 
 func Test_Model_Meta_Associations_Loading(t *testing.T) {
-	transaction(func(tx *pop.Connection) {
+	transaction(func(tx *Connection) {
 		a := require.New(t)
 
 		for _, name := range []string{"Mark", "Joe", "Jane"} {
-			user := User{Name: nulls.NewString(name)}
-			err := tx.Create(&user)
+			User := User{Name: nulls.NewString(name)}
+			err := tx.Create(&User)
 			a.NoError(err)
 
-			book := Book{UserID: nulls.NewInt(user.ID)}
+			book := Book{UserID: nulls.NewInt(User.ID)}
 			err = tx.Create(&book)
 			a.NoError(err)
 
 			if name == "Mark" {
-				song := Song{UserID: user.ID}
+				song := Song{UserID: User.ID}
 				err = tx.Create(&song)
 				a.NoError(err)
 
 				address := Address{Street: "Pop"}
 				err = tx.Create(&address)
 
-				home := UsersAddress{UserID: user.ID, AddressID: address.ID}
+				home := UsersAddress{UserID: User.ID, AddressID: address.ID}
 				err = tx.Create(&home)
 			}
 		}
 
-		users := Users{}
-		tx.All(&users)
+		Users := Users{}
+		tx.All(&Users)
 
-		mt := (&pop.Model{Value: &users}).Meta()
+		mt := (&Model{Value: &Users}).Meta()
 		err := mt.LoadDirect(tx, "has_many")
 		a.NoError(err)
 
 		err = mt.LoadDirect(tx, "has_one")
 		a.NoError(err)
 
-		// err = pop.LoadBidirect(&users, tx, "many_to_many")
+		// err = LoadBidirect(&Users, tx, "many_to_many")
 		// a.NoError(err)
 
-		a.Equal(1, len(users[0].Books))
-		a.Equal(users[0].ID, users[0].FavoriteSong.UserID)
-		a.Zero(users[1].FavoriteSong.UserID)
+		a.Equal(1, len(Users[0].Books))
+		a.Equal(Users[0].ID, Users[0].FavoriteSong.UserID)
+		a.Zero(Users[1].FavoriteSong.UserID)
 
 		books := Books{}
 		err = tx.All(&books)
 		a.NoError(err)
 
-		mt = (&pop.Model{Value: &books}).Meta()
+		mt = (&Model{Value: &books}).Meta()
 		mt.LoadIndirect(tx, "belongs_to")
-		a.Equal(users[0].ID, books[0].User.ID)
-		a.Equal(users[1].ID, books[1].User.ID)
-		a.Equal(users[2].ID, books[2].User.ID)
+		a.Equal(Users[0].ID, books[0].User.ID)
+		a.Equal(Users[1].ID, books[1].User.ID)
+		a.Equal(Users[2].ID, books[2].User.ID)
 	})
 }
